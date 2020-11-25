60a4516426348747537e81c2f84e323043e45911
vivek0206 <vickytinwal@gmail.com>
2020-11-25 12:32:28 +0530

    Camera added

--- lib/screens/Emergency.dart
+++ lib/screens/Emergency.dart
@@ -1,6 +1,12 @@
 
+
+
+import 'dart:io';
+
+import 'package:image_picker/image_picker.dart';
 import 'package:sale_spot/classes/user.dart';
 import 'package:flutter/material.dart';
+import 'package:sale_spot/services/toast.dart';
 
 class Emergency extends StatefulWidget {
   final User _user;
@@ -15,6 +21,8 @@ class _EmergencyState extends State<Emergency> {
   final User _user;
   _EmergencyState(this._user);
 
+  List<File> _images=[];
+
   @override
   Widget build(BuildContext context) {
 
@@ -25,9 +33,33 @@ class _EmergencyState extends State<Emergency> {
       body:Column(
         children: <Widget>[
           Text("hi i am vicky"),
+          RaisedButton(
+            shape: new RoundedRectangleBorder(
+                borderRadius: new BorderRadius.circular(18.0),
+                side: BorderSide(color: Colors.black)),
+            color: Colors.black,
+            textColor: Colors.white,
+            child:Icon(Icons.camera),
+            onPressed: (){_imagePickCamera();},
+          ),
         ],
       )
     );
+
+  }
+
+  _imagePickCamera() async {
+    if(_images.length>=2){
+      toast('Max images selected');
+      return;
+    }
+    var image =await ImagePicker.pickImage(source: ImageSource.camera);
+    toast('Please wait');
+    // _cropImage(image);
+    return;
+
+
+
 
   }
 }
